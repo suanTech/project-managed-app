5d52a5bc46bbed6372469e861541bf434e0c42c5
Suan Choi <siasiarhdwn@gmail.com>
2023-04-07 11:56:55 +0800

    feat: create register/signin api endpoint
    - create prisma db user with hashpassword
    - save jwt token in cookie

---
+++ pages/api/register.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@/lib/db";
+import { createJWT, hashPassword } from "@/lib/auth";
+import {serialize} from "cookie";
+
+export default async function Register(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const user = await db.user.create({
+        data: {
+          email: req.body.email,
+          password: await hashPassword(req.body.password),
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+        },
+      });
+      const jwt = await createJWT(user);
+      res
+      .setHeader(
+        "Set-Cookie",
+        serialize(process.env.COOKIE_NAME!, jwt, {
+          httpOnly: true,
+          path: "/",
+          maxAge: 60 * 60 * 24 * 7
+        })
+      )
+      .status(201).end();
+    } catch(err) {
+      res.status(403).json({err: "Error has occured while processing your request"})
+    }
+  } 
+}
---
+++ pages/api/signin.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@/lib/db";
+import { comparePasswords, createJWT, hashPassword } from "@/lib/auth";
+import {serialize} from "cookie";
+
+export default async function Signin(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const user = await db.user.findUnique({
+        where: {
+          email: req.body.email
+        }
+      })
+      if(!user) res.status(401).json({error: "Invalid Login"});
+      else {
+        const valid = await comparePasswords(req.body.password, user.password)
+        if(!valid) res.status(401).json({error: "Invalid Password"})
+        const jwt = await createJWT(user);
+        res.setHeader(
+          "Set-Cookie",
+          serialize(process.env.COOKIE_NAME, jwt, {
+            httpOnly: true,
+            path: '/',
+            maxAge: 60 * 60 * 24 * 7
+          })
+        ).status(201).end();
+      }
+    } catch(err) {
+      res.status(401).json({err: "Error has occured while processing your request"})
+    }
+  }
+}
