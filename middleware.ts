485783b8965084b3cb446a793411a64df0d0887d
Suan Choi <siasiarhdwn@gmail.com>
2023-04-12 19:25:24 +0800

    fix: change redirect routes for index page access

--- middleware.ts
+++ middleware.ts
@@ -9,35 +9,33 @@ const validateJWT = async (jwt: string) => {
     new TextEncoder().encode(process.env.JWT_SECRET)
   );
   return payload;
-}
-export default async function middleware(
-  req: NextRequest,
-  res: NextResponse
+};
+export default async function middleware(req: NextRequest, res: NextResponse) {
+  const { pathname } = req.nextUrl;
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.startsWith("/static") ||
+    pathname.startsWith("/signin") ||
+    pathname.startsWith("/register") ||
+    PUBLIC_FILE.test(pathname)
   ) {
-    const {pathname} = req.nextUrl;
-    if( 
-      pathname.startsWith("/_next") ||
-      pathname.startsWith("/api") ||
-      pathname.startsWith("/static") ||
-      pathname.startsWith("/signin") ||
-      pathname.startsWith("/register") ||
-      PUBLIC_FILE.test(pathname)
-    ) {
-      return NextResponse.next();
-    }
-    const jwt = req.cookies.get(process.env.COOKIE_NAME);
-    if(!jwt) {
-      req.nextUrl.pathname = "/signin";
-      return NextResponse.redirect(req.nextUrl)
-    } 
-    try {
-      await validateJWT(jwt.value);
-      return NextResponse.next();
-    } catch (err) {
-      console.error(err);
-      req.nextUrl.pathname = "/signin";
+    return NextResponse.next();
+  }
+  const jwt = req.cookies.get(process.env.COOKIE_NAME);
+  if (!jwt) {
+    req.nextUrl.pathname = "/signin";
+    return NextResponse.redirect(req.nextUrl);
+  }
+  try {
+    await validateJWT(jwt.value);
+    if (pathname === "/") {
+      req.nextUrl.pathname = "/home";
       return NextResponse.redirect(req.nextUrl);
-
     }
-
+    return NextResponse.next();
+  } catch (err) {
+    req.nextUrl.pathname = "/signin";
+    return NextResponse.redirect(req.nextUrl);
+  }
 }
